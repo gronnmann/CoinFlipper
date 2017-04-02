9a76a4f284df07b2095aea08dd2e352ca81dadc6
gronnmann <gronnmannthecoder@gmail.com>
2017-04-02 20:15:27 +0200

    1.83

--- CoinFlipper/src/io/github/gronnmann/coinflipper/bets/BettingTimer.java
+++ CoinFlipper/src/io/github/gronnmann/coinflipper/bets/BettingTimer.java
@@ -1,19 +1,41 @@
 package io.github.gronnmann.coinflipper.bets;
 
+import java.util.ArrayList;
+
 import org.bukkit.scheduler.BukkitRunnable;
 
+import io.github.gronnmann.coinflipper.Main;
 import io.github.gronnmann.coinflipper.gui.SelectionScreen;
 
 public class BettingTimer extends BukkitRunnable{
 	public void run(){
 		if (BettingManager.getManager().getBets().isEmpty())return;
+		
+		ArrayList<Bet> toRemove = new ArrayList<Bet>();
+		
+		
 		for (Bet b : BettingManager.getManager().getBets()){
 			b.setTimeRemaining(b.getTimeRemaining()-1);
 			
 			if (b.getTimeRemaining() == 0){
-				BettingManager.getManager().removeBet(b);
+				toRemove.add(b);
 			}
 		}
+		
+		
+		
+		
+		for (Bet b : toRemove){
+			
+			Main.getEcomony().depositPlayer(b.getPlayer(), b.getAmount());
+			
+			BettingManager.getManager().removeBet(b);
+		}
+		
+		
+		
+		
+		
 		SelectionScreen.getInstance().refreshGameManager();
 	}
 }
